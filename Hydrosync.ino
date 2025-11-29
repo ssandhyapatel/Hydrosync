25090c0008bf6f1b41436e2cbc3f471cfb394d33
Sandhya Patel <001sandhyapatel@gmail.com>
2025-11-29 11:44:19 +0530

    Update Hydrosync.ino

--- Hydrosync.ino
+++ Hydrosync.ino
@@ -6,131 +6,104 @@
 #include <MAX30100_PulseOximeter.h> 
 
 
-#define SDA_PIN 21
-#define SCL_PIN 22
+#define SDA_PIN 21       // I2C SDA
+#define SCL_PIN 22       // I2C SCL
+#define GSR_PIN 34       // Analog Input (GSR)
+#define LM35_PIN 32      // Analog Input (Temp)
 
-
-#define GSR_PIN 34    
-#define LM35_PIN 32   
-
-#define LM35_ADC_SCALE (3300.0 / 4095.0) 
-#define LM35_CONVERSION_FACTOR (LM35_ADC_SCALE / 10.0) 
-
-
-PulseOximeter pox;
-
-#define REPORTING_PERIOD_MS 100 
-
-
-#define DEVICE_NAME         "HydroSync_Wearable"
+#define REPORTING_PERIOD_MS 1000 
 #define SERVICE_UUID        "0000ffe0-0000-1000-8000-00805f9b34fb"
 #define CHARACTERISTIC_UUID "0000ffe1-0000-1000-8000-00805f9b34fb"
 
 BLECharacteristic *pCharacteristic;
+PulseOximeter pox;
 bool deviceConnected = false;
 unsigned long lastReportTime = 0;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
-      Serial.println("BLE Client Connected.");
+      Serial.println("App Connected!");
     }
-
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
-      Serial.println("BLE Client Disconnected. Starting advertising...");
-      BLEDevice::startAdvertising();
+      Serial.println("App Disconnected. Restarting Scan...");
+      BLEDevice::startAdvertising(); 
     }
 };
-
-
-//  SENSOR READING 
-
-
-float readSkinTemperature() {
-
-  int rawADC = analogRead(LM35_PIN);
-  
-
-  float temperatureC = (float)rawADC * LM35_CONVERSION_FACTOR; 
-  return temperatureC;
-}
-
-int readGSR() {
-
-  int rawGSR = analogRead(GSR_PIN);
-
-  return rawGSR;
-}
-
 
 void setup() {
   Serial.begin(115200);
 
   Wire.begin(SDA_PIN, SCL_PIN);
-
-
-  analogReadResolution(12);
+  analogReadResolution(12); 
 
   if (!pox.begin()) {
-    Serial.println("FATAL: PPG/HRV Sensor (MAX30100) not found or failed initialization.");
+    Serial.println("FAILED: MAX30100 not found! Check wiring.");
   } else {
-    Serial.println("MAX30100 initialized successfully.");
-
+    Serial.println("MAX30100 Initialized.");
     pox.setIRLedCurrent(PulseOximeter::MAX_IR_CURRENT); 
   }
   
-  // BLE
-  BLEDevice::init(DEVICE_NAME); 
+  BLEDevice::init("HydroSync_Wearable"); 
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
   
   BLEService *pService = pServer->createService(SERVICE_UUID);
-
   pCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID,
     BLECharacteristic::PROPERTY_NOTIFY
   );
-
   pCharacteristic->addDescriptor(new BLE2902());
   pService->start();
 
-  //  Advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
   BLEDevice::startAdvertising();
-
-  Serial.println(String(DEVICE_NAME) + " is advertising...");
+  
+  Serial.println("HydroSync Ready & Advertising...");
 }
 
 void loop() {
 
   pox.update();
 
-  if (deviceConnected && (millis() - lastReportTime > 2000)) { 
+  if (millis() - lastReportTime > REPORTING_PERIOD_MS) { 
   
-    float temp = readSkinTemperature(); 
-    int gsrRaw = readGSR();             
+    int rawGSR = analogRead(GSR_PIN);
+    
+    float tempC = (analogRead(LM35_PIN) * 3300.0 / 4095.0) / 10.0;
+    
     float heartRate = pox.getHeartRate(); 
 
-    String data = String(heartRate, 1) + "," + 
-                  String(temp, 2) + "," + 
-                  String(gsrRaw);
+    Serial.print("HR: "); Serial.print(heartRate);
+    Serial.print(" | Temp: "); Serial.print(tempC);
+    Serial.print(" | GSR: "); Serial.println(rawGSR);
 
-    // Print
-    Serial.println("--- HydroSync Data Packet ---");
-    Serial.println("HR: " + String(heartRate, 1) + " BPM");
-    Serial.println("Temp: " + String(temp, 2) + " C");
-    Serial.println("GSR: " + String(gsrRaw) + " raw");
-    
-    //  Notify 
-    pCharacteristic->setValue(data.c_str());
-    pCharacteristic->notify();
+   
+    if (deviceConnected) {
+        uint8_t packet[9];
+
+        
+        packet[0] = 0xAA; packet[1] = 0xAA; packet[2] = 0xAA; packet[3] = 0xAA;
+
+   
+        packet[4] = (uint8_t)heartRate;
+
+        packet[5] = (uint8_t)(rawGSR & 0xFF);
+        packet[6] = (uint8_t)((rawGSR >> 8) & 0xFF);
+
+        
+        int16_t tempScaled = (int16_t)(tempC * 100);
+        packet[7] = (uint8_t)(tempScaled & 0xFF);
+        packet[8] = (uint8_t)((tempScaled >> 8) & 0xFF);
+
+      
+        pCharacteristic->setValue(packet, 9);
+        pCharacteristic->notify();
+    }
     
     lastReportTime = millis();
-  }
-
-  if (!deviceConnected) {
-    delay(500); 
   }
 }
