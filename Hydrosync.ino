e3e6d14132842667f4365cc5b81893a809f30029
Sandhya Patel <001sandhyapatel@gmail.com>
2026-01-13 00:12:27 +0530

    Update Hydrosync.ino

--- Hydrosync.ino
+++ Hydrosync.ino
@@ -1,109 +1,102 @@
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLEServer.h>
-#include <BLE2902.h>
-#include <Wire.h> 
-#include <MAX30100_PulseOximeter.h> 
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h" 
 
 
-#define SDA_PIN 21       // I2C SDA
-#define SCL_PIN 22       // I2C SCL
+const char* SSID     = "YOUR_WIFI_NAME";      
+const char* PASSWORD = "YOUR_WIFI_PASSWORD";  
+const char* HOST_IP  = "192.168.1.100";       
+const int HOST_PORT  = 5005;
+const char* DEVICE_ID = "hydrosync_01";
+
+
+
+#define SDA_PIN 21
+#define SCL_PIN 22
 #define GSR_PIN 34       // Analog Input (GSR)
 #define LM35_PIN 32      // Analog Input (Temp)
 
-#define REPORTING_PERIOD_MS 1000 
-#define SERVICE_UUID        "0000ffe0-0000-1000-8000-00805f9b34fb"
-#define CHARACTERISTIC_UUID "0000ffe1-0000-1000-8000-00805f9b34fb"
 
-BLECharacteristic *pCharacteristic;
 PulseOximeter pox;
-bool deviceConnected = false;
-unsigned long lastReportTime = 0;
+WiFiUDP Udp;
 
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-      Serial.println("App Connected!");
-    }
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-      Serial.println("App Disconnected. Restarting Scan...");
-      BLEDevice::startAdvertising(); 
-    }
-};
+// --- TIMERS ---
+unsigned long lastUdpSend = 0;
+unsigned long packetCount = 0;
+
+//  CALLBACK 
+
+void onBeatDetected() {
+    
+}
 
 void setup() {
   Serial.begin(115200);
 
+  // 1. Initialize Sensors
   Wire.begin(SDA_PIN, SCL_PIN);
-  analogReadResolution(12); 
-
+  analogReadResolution(12); // ESP32 default is 12-bit (0-4095)
+  
+  Serial.print("Initializing MAX30100...");
   if (!pox.begin()) {
-    Serial.println("FAILED: MAX30100 not found! Check wiring.");
+    Serial.println("FAILED: MAX30100 not found!");
+    Serial.println("Check wiring: SDA->21, SCL->22");
+    
   } else {
-    Serial.println("MAX30100 Initialized.");
-    pox.setIRLedCurrent(PulseOximeter::MAX_IR_CURRENT); 
+    Serial.println("SUCCESS");
+    pox.setIRLedCurrent(PulseOximeter::MAX_IR_CURRENT);
+    pox.setOnBeatDetectedCallback(onBeatDetected);
   }
-  
-  BLEDevice::init("HydroSync_Wearable"); 
-  BLEServer *pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-  
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  pCharacteristic = pService->createCharacteristic(
-    CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_NOTIFY
-  );
-  pCharacteristic->addDescriptor(new BLE2902());
-  pService->start();
 
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  BLEDevice::startAdvertising();
-  
-  Serial.println("HydroSync Ready & Advertising...");
+
+  Serial.print("Connecting to WiFi: ");
+  Serial.println(SSID);
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi Connected!");
+  Serial.print("Device IP: "); Serial.println(WiFi.localIP());
+  Serial.print("Target IP: "); Serial.println(HOST_IP);
 }
 
 void loop() {
+ 
+  pox.update(); 
 
-  pox.update();
-
-  if (millis() - lastReportTime > REPORTING_PERIOD_MS) { 
-  
+  if (millis() - lastUdpSend > 50) {
+    
+    // 1. Read Analog Sensors
     int rawGSR = analogRead(GSR_PIN);
     
+  
     float tempC = (analogRead(LM35_PIN) * 3300.0 / 4095.0) / 10.0;
     
+   
     float heartRate = pox.getHeartRate(); 
+    float spo2 = pox.getSpO2(); 
 
-    Serial.print("HR: "); Serial.print(heartRate);
-    Serial.print(" | Temp: "); Serial.print(tempC);
-    Serial.print(" | GSR: "); Serial.println(rawGSR);
-
-   
-    if (deviceConnected) {
-        uint8_t packet[9];
-
-        
-        packet[0] = 0xAA; packet[1] = 0xAA; packet[2] = 0xAA; packet[3] = 0xAA;
-
-   
-        packet[4] = (uint8_t)heartRate;
-
-        packet[5] = (uint8_t)(rawGSR & 0xFF);
-        packet[6] = (uint8_t)((rawGSR >> 8) & 0xFF);
-
-        
-        int16_t tempScaled = (int16_t)(tempC * 100);
-        packet[7] = (uint8_t)(tempScaled & 0xFF);
-        packet[8] = (uint8_t)((tempScaled >> 8) & 0xFF);
-
-      
-        pCharacteristic->setValue(packet, 9);
-        pCharacteristic->notify();
-    }
+  
+    sendPacket(heartRate, tempC, rawGSR, spo2);
     
-    lastReportTime = millis();
+    lastUdpSend = millis();
   }
+}
+
+void sendPacket(float hr, float temp, int gsr, float spo2) {
+  char buffer[512]; 
+  unsigned long now = millis();
+  
+  snprintf(buffer, sizeof(buffer), 
+    "{\"device_id\":\"%s\",\"stream\":\"wearable\",\"sample_index\":%lu,\"timestamp_monotonic_ms\":%lu,\"values\":[%.2f,%.2f,%d,%.1f]}",
+    DEVICE_ID, packetCount++, now, hr, temp, gsr, spo2
+  );
+
+
+  Udp.beginPacket(HOST_IP, HOST_PORT);
+  Udp.print(buffer);
+  Udp.endPacket();
 }
